da35addff900a92f7e7521e4cb733b2c151513da
mart-anthony-stark <martsalazar0@gmail.com>
2021-11-23 23:18:27 +0800

    Add route watcher

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,18 @@
 <template>
-  <nav class="p-2 text-white flex items-center justify-between px-6 fixed top-0 w-full z-50">
+  <nav
+    class="
+      p-2
+      text-white
+      flex
+      items-center
+      justify-between
+      px-6
+      fixed
+      top-0
+      w-full
+      z-50
+    "
+  >
     <div class="flex gap-24 items-center">
       <img src="/images/logo.svg" alt="TechZone Logo" class="w-32" />
       <div class="links flex gap-12">
@@ -8,11 +21,37 @@
         <NuxtLink to="/about">About</NuxtLink>
       </div>
     </div>
-    <img src="/images/cart.svg" alt="cart icon" class="w-8" />
+    <div class="flex gap-8">
+      <img src="/images/cart.svg" alt="cart icon" class="w-8" />
+      <NuxtLink to="/" v-if="logged"
+        ><button class="logout-btn uppercase px-4 py-2 rounded-lg">
+          Logout
+        </button></NuxtLink
+      >
+    </div>
   </nav>
 </template>
+<script>
+export default {
+  watch: {
+    $route() {
+      console.log(this.$route.path)
+      if (this.$route.path !== '/') this.logged = true
+      else this.logged = false
+    },
+  },
+  data() {
+    return {
+      logged: true,
+    }
+  },
+}
+</script>
 <style scoped>
 nav {
   background: var(--c-pri);
+}
+.logout-btn {
+  background: var(--c-accent);
 }
 </style>
