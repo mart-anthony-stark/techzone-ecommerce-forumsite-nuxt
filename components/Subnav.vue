37d80a1566ea33b8310a4b1ded340ef5cbba0e4a
mart-anthony-stark <martsalazar0@gmail.com>
2021-11-23 23:19:22 +0800

    Add shop sub navigation

---
+++ components/Subnav.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="subnav">
+    <NuxtLink to="/gadgets/phones">
+      <button class="link-btn text-white rounded-xl px-4 py-2">
+        Mobile Phones
+      </button></NuxtLink
+    >
+    <NuxtLink to="/gadgets/phones">
+      <button class="link-btn text-white rounded-xl px-4 py-2">
+        Laptops
+      </button></NuxtLink
+    >
+    <NuxtLink to="/gadgets/phones"
+      ><button class="link-btn text-white rounded-xl px-4 py-2">
+        Tablets
+      </button></NuxtLink
+    >
+    <NuxtLink to="/gadgets/phones"
+      ><button class="link-btn text-white rounded-xl px-4 py-2">
+        Accessories
+      </button></NuxtLink
+    >
+  </div>
+</template>
+<style scoped>
+.link-btn {
+  background: var(--c-pri);
+}
+</style>
