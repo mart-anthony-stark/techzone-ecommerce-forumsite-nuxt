08f62e9e1aa18fe44885fbe191c9f676e2448e15
mart-anthony-stark <martsalazar0@gmail.com>
2021-11-24 13:38:50 +0800

    Change meta tag title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'techzen-poc',
+    title: 'TechZone POC',
     htmlAttrs: {
       lang: 'en'
     },
