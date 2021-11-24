9e18f05a5d001de9d5c6b2997d40cf76067193b2
mart-anthony-stark <martsalazar0@gmail.com>
2021-11-24 20:57:53 +0800

    Add products store

---
+++ store/products.js
@@ -0,0 +1,33 @@
+export const state = () => ({
+  phones: [
+      { model: 'APPLE iPhone 11', photo: '/images/phones/1.svg' },
+      {
+        model: 'SAMSUNG Galaxy A72',
+        photo: '/images/phones/2.svg',
+      },
+  ],
+  laptops: [
+    { model: 'ACER Aspire 3 A314-22-R4U5', photo: '/images/laptops/1.svg' },
+    {
+      model: 'ACER Aspire 3 A315-57G-59HR',
+      photo: '/images/laptops/2.svg',
+    },
+  ],
+  tablets: [
+    { model: 'AMAZON Fire HD 10"', photo: '/images/tablets/1.svg' },
+    {
+      model: 'APPLE iPad 8 10.2" (2020) Wi-Fi',
+      photo: '/images/tablets/2.svg',
+    },
+  ],
+  accessories: [
+    {
+      model: 'ANKER SoundCore Space NC B2B',
+      photo: '/images/accessories/1.svg',
+    },
+    {
+      model: 'DELUX M556BU Entry Level Gaming Mouse Cool Light',
+      photo: '/images/accessories/2.svg',
+    },
+  ],
+})
