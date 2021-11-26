c4277e4a7ff5e17eae1da0c0b4a902178d87771a
mart-anthony-stark <martsalazar0@gmail.com>
2021-11-26 22:52:11 +0800

    Add dynamic view product page

---
+++ pages/_gadget/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mt-24">
+    <div @click="$router.go(-1)">Back</div>
+    <div class="card">
+      <img :src="item.photo" />
+      {{ item.model }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    item() {
+      const paths = this.$route.path.split('/').filter((p) => p !== '')
+      const gadget = paths[0]
+      const id = paths[1]
+      return this.$store.state.products[`${gadget}s`][id]
+    },
+  },
+  data() {
+    return {}
+  },
+  beforeCreate() {
+    // const paths = this.$route.path.split('/').filter((p) => p !== '')
+    // console.log(this.$store.state.products.laptops)
+    // this.item = this.$store.state.products[paths[0]]
+  },
+}
+</script>
