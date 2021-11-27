4f1fc26ba78c80bb398ba8df110e97912cfab988
mart-anthony-stark <martsalazar0@gmail.com>
2021-11-27 21:58:00 +0800

    Add add to cart functionality

--- pages/_gadget/_id.vue
+++ pages/_gadget/_id.vue
@@ -16,9 +16,23 @@
       <h1>Back</h1>
     </div>
 
-    <div class="card">
+    <div class="container grid grid-cols-2 place-items-center">
       <img :src="item.photo" />
-      <h1 class="text-xl extrabold">{{ item.model }}</h1>
+      <div class="card p-12 pl-0">
+        <h1 class="text-xl extrabold">{{ item.model }}</h1>
+        <h3>{{ item.priceStr }}</h3>
+        <p class="mt-8">{{ item.description }}</p>
+        <div class="mt-6">
+          <h2 class="text-blue-500 text-lg">Specifications:</h2>
+          <h3 v-for="bullet in item.specs" :key="bullet">{{ bullet }}</h3>
+        </div>
+        <button
+          @click="addThis"
+          class="bg-pri text-white px-12 py-2 rounded mt-4"
+        >
+          Add to cart
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -32,9 +46,27 @@ export default {
       const id = paths[1]
       return this.$store.state.products[`${gadget}s`][id]
     },
+    cartItems() {
+      return this.$store.state.cart.cart
+    },
+    isAuth() {
+      return this.$store.state.auth.isLogged
+    },
   },
   data() {
     return {}
+  },
+  methods: {
+    addThis() {
+      const exist = this.cartItems.find((el) => el.model === this.item.model)
+
+      if (!this.isAuth) return
+      if (exist) {
+        console.log('already in cart')
+      } else {
+        this.$store.commit('cart/addToCart', this.item)
+      }
+    },
   },
 }
 </script>
