990a7312fd839b353da0cf1858b76f2ccb8770d9
mart-anthony-stark <martsalazar0@gmail.com>
2021-11-27 21:58:13 +0800

    Add model

--- store/cart.js
+++ store/cart.js
@@ -1,17 +1,30 @@
 export const state = () => ({
-  cart: [],
+  cart: [
+    {
+      model: 'APPLE iPhone 11',
+      photo: '/images/phones/1.svg',
+      priceStr: '₱ 34,990.00',
+      price: 34990.0,
+      specs: ['4GB RAM', '64/128/256GB ROM', 'A13 Bionic', '4G Hexa Core'],
+      description:
+        'The iPhone 11 has an edge-to-edge display with slim bezels and no Home button, adopting a notch at the top for the TrueDepth camera system.',
+    },
+  ],
 })
 
 export const mutations = {
   addToCart(state, item) {
     item.quantity = 1
-    state.push(item)
+    state.cart.push(item)
   },
-  addItem(state, model) {
+  addQuantity(state, model) {
     state.cart.forEach((element) => {
       if (element.model === model) {
         element.quantity++
       }
     })
+  },
+  removeItem(state, model) {
+    state.cart = state.cart.filter((item) => item.model !== model)
   },
 }
