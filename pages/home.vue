e7dc0fe5779ef1e721e6acd3b3ca0bc1aa795ce0
mart-anthony-stark <martsalazar0@gmail.com>
2021-11-28 20:03:13 +0800

    Fix posts array, moved to vuex store

--- pages/home.vue
+++ pages/home.vue
@@ -73,57 +73,12 @@ export default {
     return {
       createShown: false,
       search: '',
-      posts: [
-        {
-          author: 'Melinda Gates',
-          authorProfile: '/images/avatars/1.svg',
-          title: 'High-Speed Internet Connection is Coming to Remote Areas',
-          body: 'British firm OneWeb recently launched 34 satellites into orbit from a spaceport in Kazakhstan, increasing its in-orbit constellation to 322 satellites. The satellites are intended to provide high-speed internet coverage in areas that are underserved by traditional methods. It’s part of a new wave of technologies that could help bridge the digital divide.',
-          comments: [],
-          totalComments: 5,
-          liked: false,
-          disliked: true,
-          totalLikes: 56,
-        },
-        {
-          author: 'John Roy Calimlim',
-          authorProfile: '/images/avatars/2.svg',
-          title:
-            'Arista, Cisco, Huawei, Juniper Networks, and Nokia Launch New MANRS Equipment Vendor Program to Improve Routing Security Worldwide',
-          body: 'Founding members of the MANRS Initiative will tackle the systemic vulnerabilities of global routing security through groundbreaking collaborative action.',
-          comments: [],
-          totalComments: 8,
-          liked: true,
-          disliked: false,
-          totalLikes: 27,
-        },
-        {
-          author: 'Sarah Ford',
-          authorProfile: '/images/avatars/3.svg',
-          title: 'An Interactive Guide to Keyframe Animations',
-          body: "CSS keyframe animations are incredibly flexible and powerful, but they’re also a bit weird. In this deep-dive tutorial, we'll learn how CSS keyframes work from the ground up, and see how to use them to build high-quality animations.",
-          comments: [],
-          totalComments: 34,
-          liked: true,
-          disliked: false,
-          totalLikes: 88,
-        },
-        {
-          author: 'John Ray',
-          authorProfile: '/images/avatars/4.svg',
-          title:
-            'NanoPen 02 packs rugged durability in a small, tactical frame for minimalist EDC',
-          body: 'In the market for a new tactical pen? Update your EDC kit with the improved NanoPen 02. It features a slew of enhancements, from a quick-release cap to copper housing. Read on to learn more.',
-          comments: [],
-          totalComments: 20,
-          liked: false,
-          disliked: false,
-          totalLikes: 14,
-        },
-      ],
     }
   },
   computed: {
+    posts() {
+      return this.$store.state.posts.posts
+    },
     filteredPost() {
       const regex = new RegExp(this.search, 'i')
       return this.posts.filter((post) => {
@@ -137,23 +92,10 @@ export default {
   },
   methods: {
     up(index) {
-      if (!this.posts[index].liked) {
-        this.posts[index].liked = true
-        this.posts[index].totalLikes++
-        this.posts[index].disliked = false
-      } else {
-        this.posts[index].liked = false
-        this.posts[index].totalLikes--
-      }
+      this.$store.commit('posts/up', index)
     },
     down(index) {
-      if (!this.posts[index].disliked) {
-        this.posts[index].disliked = true
-        this.posts[index].totalLikes--
-        this.posts[index].liked = false
-      } else {
-        this.posts[index].disliked = false
-      }
+      this.$store.commit('posts/down', index)
     },
   },
 }
