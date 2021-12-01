4df584bdb4bf60584a8d55b5f244a937f2869947
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-02 00:24:47 +0800

    Add item link

--- store/products.js
+++ store/products.js
@@ -9,6 +9,7 @@ export const state = () => ({
       specs: ['4GB RAM', '64/128/256GB ROM', 'A13 Bionic', '4G Hexa Core'],
       description:
         'The iPhone 11 has an edge-to-edge display with slim bezels and no Home button, adopting a notch at the top for the TrueDepth camera system.',
+      link: '/phone/0',
     },
     {
       model: 'SAMSUNG Galaxy A72',
@@ -18,6 +19,7 @@ export const state = () => ({
       price: 23990.0,
       specs: ['6.7-inch Screen', '8GB RAM', '256GB Storage'],
       description: `The Samsung Galaxy A72 is a good-looking phone, but so are others at around this price, which makes any of them a better pick. There's the Mi 10T Pro and OnePlus 8T, both of which deliver better gaming performance and cameras.`,
+      link: '/phone/1',
     },
   ],
   laptops: [
@@ -38,6 +40,7 @@ export const state = () => ({
       ],
       description:
         'Acer Aspire 3 A314-22-R4U5-Black AMD Ryzen 5 3500U / 4GB / 128GB SSD + 1TB HDD / 14 Display / AMD Radeon Vega 8 Mobile Graphics / Windows 10',
+      link: '/laptop/0',
     },
     {
       model: 'ACER Aspire 3 A315-57G-59HR',
@@ -52,6 +55,7 @@ export const state = () => ({
       ],
       description:
         'Home Computers Notebooks & Laptops Acer Aspire 3 A315-57G-59HR ( Charcoal Black) Core-i5 10th Gen.',
+      link: '/laptop/1',
     },
   ],
   tablets: [
@@ -70,6 +74,7 @@ export const state = () => ({
       ],
       description:
         'Fast and responsive - powerful octa-core processor and 3 GB RAM. 50- more RAM than previous generation.',
+      link: '/tablet/0',
     },
     {
       model: 'APPLE iPad 8 10.2" (2020) Wi-Fi',
@@ -86,6 +91,7 @@ export const state = () => ({
       ],
       description: `Also known as Apple iPad 8th Gen, Apple iPad (8th generation.
       Wi-Fi + Cellular & GPS: A2428, A2429 (Global), A2430 (China). Wi-Fi only, w/o GPS: A2270 (Global)`,
+      link: '/tablet/1',
     },
   ],
   accessories: [
@@ -98,6 +104,7 @@ export const state = () => ({
       specs: ['40 mm drivers'],
       description: `Hybrid active noise cancellation technology blocks out up to 93% of low-frequency engine noise and up to 96% of high-frequency sounds such as voices and vehicle horns.
       Simply tap the button on the right ear-cup to effortlessly answer calls. A built-in microphone is equipped with cVc noise cancelling technology suppress ambient noise and elevate quality.`,
+      link: '/accessorie/0',
     },
     {
       model: 'DELUX M556BU Entry Level Gaming Mouse Cool Light',
@@ -108,6 +115,7 @@ export const state = () => ({
       specs: ['HXSJ T90', '3 Modes BT', '3.0 + 5.0 + 2.4G'],
       description:
         'Wireless Chargable RGB Lighting Color Mouse Mice for Game Office with Adjustable 800-3200 5 Level DPI',
+      link: '/accessorie/1',
     },
   ],
 })
