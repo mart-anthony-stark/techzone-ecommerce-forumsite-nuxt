3a83804c3b1fb8b8905e10a1224af9f2baa4e5e9
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-02 21:54:42 +0800

    Add order tracker

--- pages/order-tracker.vue
+++ pages/order-tracker.vue
@@ -1,12 +1,79 @@
 <template>
   <div class="section mt-24 px-8">
-    <h1>Order Tracker</h1>
-    <div>
-      <div>
-        <h1>Order XCIFGD1</h1>
-        <div>
-          <h3>Expected date: 12/25/21</h3>
-          <h4>USPS 216541064654</h4>
+    <h1 class="font-extrabold text-center text-4xl mb-8 text-blue-500">
+      Order Tracker
+    </h1>
+    <div class="flex justify-center">
+      <div class="card">
+        <div class="flex justify-between items-start">
+          <h1 class="text-2xl">
+            Order: <span class="text-sec font-extrabold">XCIFGD1</span>
+          </h1>
+          <div>
+            <h3>Expected date: <span class="font-extrabold">12/25/21</span></h3>
+            <h4>USPS <span class="font-extrabold">216541064654</span></h4>
+          </div>
+        </div>
+
+        <div class="track mt-8">
+          <ul
+            id="progress"
+            class="
+              text-center
+              flex flex-wrap
+              gap-1
+              items-center
+              justify-between
+              relative
+            "
+          >
+            <li class="active"></li>
+            <li class="active"></li>
+            <li class="active"></li>
+            <li class=""></li>
+          </ul>
+        </div>
+
+        <div class="lists flex justify-between">
+          <div class="list mt-24">
+            <div class="text-5xl text-center">
+              <fa icon="list-alt" />
+            </div>
+            <p class="text-center">
+              Order <br />
+              Processed
+            </p>
+          </div>
+
+          <div class="list mt-24">
+            <div class="text-5xl text-center">
+              <fa icon="archive" />
+            </div>
+            <p class="text-center">
+              Order <br />
+              Shipped
+            </p>
+          </div>
+
+          <div class="list mt-24">
+            <div class="text-5xl text-center">
+              <fa icon="truck" />
+            </div>
+            <p class="text-center">
+              Order <br />
+              Enroute
+            </p>
+          </div>
+
+          <div class="list mt-24">
+            <div class="text-5xl text-center">
+              <fa icon="home" />
+            </div>
+            <p class="text-center">
+              Order <br />
+              Arrived
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -18,4 +85,51 @@ export default {}
 </script>
 
 <style scoped>
+.card {
+  max-width: 900px;
+  width: 100%;
+}
+#pogress li {
+  width: 20%;
+  position: relative;
+}
+#progress li::before {
+  content: '\2713';
+  display: block;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  width: 50px;
+  align-items: center;
+  font-size: 2rem;
+  height: 50px;
+  background: var(--c-accent);
+  border-radius: 50%;
+  color: #fff;
+}
+#progress li:last-child::before {
+  content: '\2713';
+  font-weight: bold;
+  background: rgb(179, 179, 179);
+}
+#progress::before {
+  content: '';
+  position: absolute;
+  top: 20px;
+  width: 65%;
+  margin-left: 35px;
+  background: var(--c-accent);
+  height: 10px;
+  z-index: -1;
+}
+#progress::after {
+  content: '';
+  position: absolute;
+  top: 20px;
+  width: 100%;
+  margin-left: 35px;
+  background: rgb(179, 179, 179);
+  height: 10px;
+  z-index: -11;
+}
 </style>
