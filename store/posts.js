062944cadd5b22f1ee4ecf66164a0e3df5e3d44e
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-04 21:07:46 +0800

    Add username

--- store/posts.js
+++ store/posts.js
@@ -4,6 +4,7 @@ export const state = () => ({
       id: 1,
       author: 'Melinda Gates',
       authorProfile: '/images/avatars/1.svg',
+      username: 'mgates21',
       title: 'High-Speed Internet Connection is Coming to Remote Areas',
       body: 'British firm OneWeb recently launched 34 satellites into orbit from a spaceport in Kazakhstan, increasing its in-orbit constellation to 322 satellites. The satellites are intended to provide high-speed internet coverage in areas that are underserved by traditional methods. It’s part of a new wave of technologies that could help bridge the digital divide.',
       comments: [],
@@ -16,6 +17,7 @@ export const state = () => ({
       id: 2,
       author: 'John Roven',
       authorProfile: '/images/avatars/2.svg',
+      username: 'jrstark',
       title:
         'Arista, Cisco, Huawei, Juniper Networks, and Nokia Launch New MANRS Equipment Vendor Program to Improve Routing Security Worldwide',
       body: 'Founding members of the MANRS Initiative will tackle the systemic vulnerabilities of global routing security through groundbreaking collaborative action.',
@@ -29,6 +31,7 @@ export const state = () => ({
       id: 3,
       author: 'Sarah Ford',
       authorProfile: '/images/avatars/3.svg',
+      username: 'sarahG',
       title: 'An Interactive Guide to Keyframe Animations',
       body: "CSS keyframe animations are incredibly flexible and powerful, but they’re also a bit weird. In this deep-dive tutorial, we'll learn how CSS keyframes work from the ground up, and see how to use them to build high-quality animations.",
       comments: [],
@@ -41,6 +44,7 @@ export const state = () => ({
       id: 4,
       author: 'John Ray',
       authorProfile: '/images/avatars/4.svg',
+      username: 'rayray23',
       title:
         'NanoPen 02 packs rugged durability in a small, tactical frame for minimalist EDC',
       body: 'In the market for a new tactical pen? Update your EDC kit with the improved NanoPen 02. It features a slew of enhancements, from a quick-release cap to copper housing. Read on to learn more.',
