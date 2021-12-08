18e1411792c4c3e2817b13301099fd1b4aec79f9
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-08 20:47:30 +0800

    Fix libraries

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -4,6 +4,6 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 Vue.use(VueGoogleMaps, {
   load: {
     key: 'AIzaSyD_6Dqn6XPCh-cuIvFvDu7ngtXDW3BKzk4',
-    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // libraries: 'places', // This is required if you use the Autocomplete plugin
   },
 })
