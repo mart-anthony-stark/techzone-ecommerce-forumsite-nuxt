09bcb8acea32326754d76d8c8054eea8ca049662
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-01 19:04:20 +0800

    Fix api key leak issue

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -3,7 +3,6 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyD_6Dqn6XPCh-cuIvFvDu7ngtXDW3BKzk4',
-    // libraries: 'places', // This is required if you use the Autocomplete plugin
+    key: process.env.GOOGLE_MAP_API_KEY,
   },
 })
